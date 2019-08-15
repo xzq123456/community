769f056f8ff8642b105c9ed4884748312509716f
xzq123456 <2270430924@qq.com>
2019-08-15 13:24:06 +0800

    first cmmmit

---
+++ src/main/java/life/majiang/community/dto/QuestionQueryDTO.java
@@ -0,0 +1,14 @@
+package life.majiang.community.dto;
+
+import lombok.Data;
+
+/**
+ * Created by codedrinker on 2019/7/1.
+ */
+@Data
+public class QuestionQueryDTO {
+    private String search;
+    private String tag;
+    private Integer page;
+    private Integer size;
+}
